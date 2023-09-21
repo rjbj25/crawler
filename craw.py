b6ed506448c07dbe137f388fa6faab4e6fd61dcb
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2023-09-21 09:17:00 -0500

    Update craw.py

--- craw.py
+++ craw.py
@@ -28,41 +28,42 @@ def get_all_pages(pages, url, domain, visited):
     except:
         print(f'Error al cargar la página {url}')
         return pages
-
-    # parse the HTML content of the website
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # find all links in the HTML content
-    links = soup.find_all('a')
-
-    # extract the URLs from the links
-    for link in links:
-        page = link.get('href')
-        if not page:
-            continue
-        if page[0] == '/':
-            page = f'https://{domain}{page}'
-        elif page[0] == 'w':
-            page = f'https://{page}'
-        if domain not in page or page in visited or page.endswith('.jpg'):
-            continue
-        #print(f'Inx {len(visited)} - Scaning: {page}')
-        df = pd.DataFrame([page], columns=['URL'])
-        # Se escribe el dataframe en un archivo csv añadiendo los datos al final del archivo
-        df.to_csv('export_all.csv', index=False, mode='a', header=False)
-        visited.add(page)
-        pages.append(page)
-
-    for page in tqdm(pages):
-        pages += get_all_pages([], page, domain, visited)
-
-    return pages
-
+    try:
+        # parse the HTML content of the website
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        # find all links in the HTML content
+        links = soup.find_all('a')
+    
+        # extract the URLs from the links
+        for link in links:
+            page = link.get('href')
+            if not page:
+                continue
+            if page[0] == '/':
+                page = f'https://{domain}{page}'
+            elif page[0] == 'w':
+                page = f'https://{page}'
+            if domain not in page or page in visited or page.endswith('.jpg'):
+                continue
+            #print(f'Inx {len(visited)} - Scaning: {page}')
+            df = pd.DataFrame([page], columns=['URL'])
+            # Se escribe el dataframe en un archivo csv añadiendo los datos al final del archivo
+            df.to_csv('export_all.csv', index=False, mode='a', header=False)
+            visited.add(page)
+            pages.append(page)
+    
+        for page in tqdm(pages):
+            pages += get_all_pages([], page, domain, visited)
+    
+        return pages
+    except:
+        return pages
 
 def main():
     # the URL of the website to crawl
-    url = 'https://www.idbinvest.org/es/proyectos?language=es'
-    domain = 'www.idbinvest.org'
+    url = 'https://grupomakro.org'
+    domain = 'grupomakro.org'
     pages = []
     # get all the URLs on the website
     pages = get_all_pages(pages, url, domain, visited)
